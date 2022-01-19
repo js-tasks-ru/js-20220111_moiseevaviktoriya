0b33a1dac43711fc79c4528e5d90bdaf12d99f8b
viktoriya_moiseeva <adxomega@yandex.ru>
2022-01-20 00:48:26 +0300

    03-objects-arrays-intro-to-testing/2-invert-object

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,14 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (!obj) { return; }
 
+  let entries = Object.entries(obj);
+  let result = {};
+
+  entries.forEach(([key, value]) => {
+    result[value] = key;
+  });
+
+  return result;
 }
