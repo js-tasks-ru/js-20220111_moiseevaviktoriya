b14e436a8be959b9492c8815976cfd930d7aa286
viktoriya_moiseeva <adxomega@yandex.ru>
2022-01-20 19:01:41 +0300

    03-objects-arrays-intro-to-testing/3-trim-symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,20 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  let arrFromString = string.split('');
+  let trimmedString = '';
 
+  if (size === 0 || !string) {
+    return '';
+  } else if (size === undefined) {
+    return string;
+  }
+
+  for (let i = 0; i < arrFromString.length; i++) {
+    if (arrFromString[i] !== arrFromString[i - size] || arrFromString[i] !== arrFromString[i - 1]) {
+      trimmedString += arrFromString[i];
+    }
+  }
+
+  return trimmedString;
 }
