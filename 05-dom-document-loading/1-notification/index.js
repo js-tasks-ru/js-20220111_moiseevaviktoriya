0dcf1652b7cb3ac6e3ca0a52c671ef89c54422c3
VikMoi <victoria.moiseeva@avangard-mb.ru>
2022-01-26 18:22:26 +0300

    05-dom-document-loading\1-notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,57 @@
 export default class NotificationMessage {
+  element = null;
+  static activeElement;
 
+  constructor(message = '', {
+    duration = 1000,
+    type = 'success'
+  } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.render();
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.getTemplate();
+    this.element = element.firstElementChild;
+  }
+
+  show(target = document.body) {
+    if (this.constructor.activeElement) {
+      this.constructor.activeElement.remove();
+    }
+
+    this.constructor.activeElement = this.element;
+    target.append(this.element);
+
+    this.destroy();
+  }
+
+  getTemplate() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.duration}ms">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  destroy() {
+    setTimeout(() => {
+      this.remove();
+    }, this.duration);
+  }
+
+  remove() {
+    this.element.remove();
+    this.constructor.activeElement.remove();
+  }
 }
