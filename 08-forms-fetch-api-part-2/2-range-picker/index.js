fa15f77985e0e24736e79ebde41de001305ffdbf
viktoriya_moiseeva <adxomega@yandex.ru>
2022-02-07 23:05:18 +0300

    08-forms-fetch-api-part-2\2-range-picker

--- 08-forms-fetch-api-part-2/2-range-picker/index.js
+++ 08-forms-fetch-api-part-2/2-range-picker/index.js
@@ -2,6 +2,9 @@ export default class RangePicker {
   element;
   subElements = {};
   isShown = false;
+  firstMonth;
+  secondMonth;
+  selected;
 
   constructor({
     from = new Date(),
@@ -13,14 +16,13 @@ export default class RangePicker {
   }
 
   render() {
-    /*let date = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
-    let options = { month: 'long'};
-    console.log(date.toLocaleDateString('ru-RU', options));*/
-
     const element = document.createElement('div');
     element.innerHTML = this.getTemplate();
     this.element = element.firstElementChild;
     this.subElements = this.getSubElements();
+    this.firstMonth = new Date(this.from.getFullYear(), this.from.getMonth());
+    this.secondMonth = new Date(this.from.getFullYear(), this.from.getMonth());
+    this.secondMonth.setMonth(this.secondMonth.getMonth() + 1);
     this.getInputs();
     this.initEventListeners();
   }
@@ -32,13 +34,7 @@ export default class RangePicker {
             <span data-element="from"></span> -
             <span data-element="to"></span>
           </div>
-          <div class="rangepicker__selector" data-element="selector">
-            <div class="rangepicker__selector-arrow"></div>
-            <div class="rangepicker__selector-control-left"></div>
-            <div class="rangepicker__selector-control-right"></div>
-            ${this.renderMonth(true)}
-            ${this.renderMonth()}
-          </div>
+          <div class="rangepicker__selector" data-element="selector"></div>
     </div>
     `;
   }
@@ -66,27 +62,45 @@ export default class RangePicker {
   }
 
   toggleOpened = () => {
-    this.isShown = !!this.isShown;
+    if (!this.isShown) {
+      this.initSelector();
+      this.isShown = true;
+    }
     this.element.classList.toggle('rangepicker_open');
+  };
+
+  initSelector() {
+    let selector = `
+        <div class="rangepicker__selector-arrow"></div>
+        <div class="rangepicker__selector-control-left"></div>
+        <div class="rangepicker__selector-control-right"></div>
+        ${this.renderMonth(this.firstMonth)}
+        ${this.renderMonth(this.secondMonth)}
+      `;
+
+    this.subElements.selector.insertAdjacentHTML('afterbegin', selector);
+
+    const prevBtn = this.element.querySelector('.rangepicker__selector-control-left');
+    const nextBtn = this.element.querySelector('.rangepicker__selector-control-right');
+
+    prevBtn.addEventListener('click', this.toggleMonth);
+    nextBtn.addEventListener('click', this.toggleMonth);
+
+    this.subElements.selector.addEventListener('click', this.select);
   }
 
-  renderMonth(isFirstMonth = false) {
-    let month;
-
-    if (isFirstMonth) {
-      month = this.from.toLocaleString('ru-RU', {month: 'long'});
-    } else {
-
-    }
+  renderMonth(month) {
+    let monthRus = month.toLocaleString('ru-RU', {month: 'long'});
+    let monthEng = month.toLocaleString('en-EN', {month: 'long'});
 
     return `
         <div class="rangepicker__calendar">
             <div class="rangepicker__month-indicator">
-                <time datetime="November">${month}</time>
+                <time datetime="${monthEng}">${monthRus}</time>
             </div>
             ${this.getWeekTemplate()}
            <div class="rangepicker__date-grid">
-                ${this.getDateTemplate()}
+                ${this.getDateTemplate(month)}
            </div>
         </div>
     `;
@@ -106,12 +120,12 @@ export default class RangePicker {
     `;
   }
 
-  getDateTemplate() {
-    let firstDate = new Date(this.from.getFullYear(), this.from.getMonth());
+  getDateTemplate(month) {
+    let firstDate = new Date(month.getFullYear(), month.getMonth());
     const arr = [];
 
-    while (firstDate.getMonth() === this.from.getMonth()) {
-      arr.push(new Date(this.from.getFullYear(), this.from.getMonth(), firstDate.getDate()));
+    while (firstDate.getMonth() === month.getMonth()) {
+      arr.push(new Date(month.getFullYear(), month.getMonth(), firstDate.getDate()));
       firstDate.setDate(firstDate.getDate() + 1);
     }
 
@@ -141,6 +155,59 @@ export default class RangePicker {
       classStr += ' rangepicker__selected-to';
     }
     return classStr;
+  }
+
+  toggleMonth = (e) => {
+    const direction = e.currentTarget.classList.contains('rangepicker__selector-control-left') ? -1 : 1;
+    this.subElements.selector.innerHTML = '';
+
+    this.firstMonth.setMonth(this.firstMonth.getMonth() + direction);
+    this.secondMonth.setMonth(this.secondMonth.getMonth() + direction);
+
+    this.initSelector();
+  };
+
+  select = (event) => {
+    const active = event.target.closest('.rangepicker__cell');
+
+    if (!active) {
+      return;
+    }
+
+    if (!this.selected) {
+      [...this.subElements.selector.querySelectorAll('.rangepicker__cell')].forEach(item => {
+        item.className = 'rangepicker__cell';
+      });
+
+      this.selected = active;
+      this.selected.classList.add('rangepicker__selected-from');
+      this.from = new Date(Date.parse(this.selected.dataset.value));
+    } else {
+      this.to = new Date(Date.parse(active.dataset.value));
+      active.classList.add('rangepicker__selected-to');
+      if (this.to - this.from < 0) {
+        this.from = this.to;
+        this.to = new Date(Date.parse(this.selected.dataset.value));
+      }
+      this.setNewRange();
+
+      this.element.dispatchEvent(new CustomEvent("date-select", {
+        detail: 'Successfully selected'
+      }));
+    }
+  };
+
+  setNewRange() {
+    this.element.classList.remove('rangepicker_open');
+    this.getInputs();
+
+    [...this.subElements.selector.querySelectorAll('.rangepicker__cell')].forEach(item => {
+      if (Date.parse(item.dataset.value) > this.from.getTime() && Date.parse(item.dataset.value) < this.to.getTime()) {
+        item.classList.add('rangepicker__selected-between');
+      }
+    });
+
+    this.selected = null;
   }
 
   destroy() {
