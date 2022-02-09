de678876d709ec636f2111a45c67daeeb709b146
VikMoi <victoria.moiseeva@avangard-mb.ru>
2022-02-09 17:58:50 +0300

    09-tests-for-frontend-apps\2-sortable-list

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -1,3 +1,44 @@
 export default class SortableList {
+  element;
 
+  constructor({items = []} = {}) {
+    this.items = items;
+    this.render();
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.getTemplate();
+    this.element = element.firstElementChild;
+    this.getList();
+    this.initEventListeners();
+  }
+
+  getTemplate() {
+    return `
+       <ul class="sortable-list"></ul>
+    `;
+  }
+
+  getList() {
+    if (this.items.length) {
+      this.items.forEach(item => {
+        this.element.append(item);
+        item.classList.add('sortable-list__item');
+      });
+    }
+  }
+
+  initEventListeners() {
+    this.element.addEventListener('dragstart', this.startDragging);
+  }
+
+  startDragging = event => {
+    const active = event.target.closest('.sortable-list__item');
+
+    if (!active) {
+      return;
+    }
+
+  }
 }
